1c43c64c3a265fe5c2d39f87eaf6f5bbe810bc15
Dan_Dude <dan@vfdworld.com>
2017-05-02 18:45:31 -0500

    Duplicate existing functionality from hacky php scripts

---
+++ MilwaukeeMakerspaceApi/Controllers/InfoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mms.Database;
+
+namespace MilwaukeeMakerspaceApi.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Index()
+        {
+			fund data = null;
+
+			using (var db = new AreaFundingDatabase())
+			{
+				var sql = @"
+					SELECT 
+						* 
+					FROM 
+						funds 
+					ORDER BY 
+						month DESC 
+					LIMIT 1;";
+
+				data = db.Single<fund>(sql);
+			}
+
+			return View(data);
+		}
+
+		public IActionResult Balances()
+        {
+			balance data = null;
+
+			using (var db = new AreaFundingDatabase())
+			{
+				var sql = @"
+				SELECT
+					*
+				FROM
+					balances
+				LIMIT 1;";
+
+				data = db.Single<balance>(sql);
+			}
+
+			return View(data);
+        }
+    }
+}
