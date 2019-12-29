d81ac4dbf91944c6fc77fe56e0352b3cb772804d
Dan_Dude <dan@vfdworld.com>
2019-12-29 00:40:31 -0600

    Added useful scripts

---
+++ SQL Scripts/report tool usage detail.sql
@@ -0,0 +1,35 @@
+SELECT  
+	m.name,
+	r.name,
+	a.attempt_time AS 'login',
+	IFNULL(
+		MIN(l.attempt_time), 
+		DATE_ADD(a.attempt_time, INTERVAL r.timeout SECOND)
+	) AS 'logout',
+	SEC_TO_TIME(TIMESTAMPDIFF(SECOND, a.attempt_time, IFNULL(
+		MIN(l.attempt_time), 
+		DATE_ADD(a.attempt_time, INTERVAL r.timeout SECOND)
+	))) AS usage_time
+FROM 
+	attempt a 
+	INNER JOIN member m 
+		ON a.member_id = m.member_id 
+	INNER JOIN reader r
+		ON a.reader_id = r.reader_id
+	LEFT JOIN attempt l
+		ON a.reader_id = l.reader_id 
+		AND l.access_granted = 1
+		AND l.attempt_time > a.attempt_time
+		AND l.attempt_time < DATE_ADD(a.attempt_time, INTERVAL r.timeout SECOND)
+WHERE 
+	a.reader_id IN (6,7,8) 
+	AND a.access_granted = 1
+	AND a.attempt_time > '2019-04-01'
+GROUP BY
+	m.name,
+	r.name,
+	a.attempt_time
+ORDER BY 
+	m.name ASC,
+	r.name ASC,
+	a.attempt_time ASC
