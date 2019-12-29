971042061a3876399d4a38b4a9b917f0605d6843
Dan_Dude <dan@vfdworld.com>
2019-12-29 00:40:04 -0600

    Switch to newer DB Library. Simplify build

---
+++ Mms.Database/generate.bat
@@ -0,0 +1,2 @@
+dotnet "%USERPROFILE%/.nuget/packages/petapoco.dbentitygenerator/0.1.0/tools/netcoreapp2.2/PetaPoco.DBEntityGenerator.dll" --config access_control.json > access_control.cs
+dotnet "%USERPROFILE%/.nuget/packages/petapoco.dbentitygenerator/0.1.0/tools/netcoreapp2.2/PetaPoco.DBEntityGenerator.dll" --config area_funding.json > area_funding.cs
