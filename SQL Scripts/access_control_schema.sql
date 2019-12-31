c3ea52518ce647d5dce76d716173b5674e50eefb
Dan_Dude <dan@vfdworld.com>
2019-12-31 14:36:57 -0600

    Update database for new reader features

--- SQL Scripts/access_control_schema.sql
+++ SQL Scripts/access_control_schema.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Community
-MySQL - 10.1.26-MariaDB-0+deb9u1 : Database - access_control
+MySQL - 10.3.18-MariaDB-0+deb10u1 : Database - access_control
 *********************************************************************
 */
 
@@ -11,59 +11,59 @@ MySQL - 10.1.26-MariaDB-0+deb9u1 : Database - access_control
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`access_control` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`access_control` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 
 /*Table structure for table `attempt` */
 
 CREATE TABLE `attempt` (
   `attempt_id` int(11) NOT NULL AUTO_INCREMENT,
-  `keycode` varchar(255) NOT NULL DEFAULT '',
+  `keycode` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `member_id` int(11) NOT NULL,
   `reader_id` int(11) NOT NULL,
-  `access_granted` tinyint(1) NOT NULL DEFAULT '0',
-  `login` tinyint(1) NOT NULL DEFAULT '0',
-  `logout` tinyint(1) NOT NULL DEFAULT '0',
+  `access_granted` tinyint(1) NOT NULL DEFAULT 0,
+  `login` tinyint(1) NOT NULL DEFAULT 0,
+  `logout` tinyint(1) NOT NULL DEFAULT 0,
+  `action` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `attempt_time` datetime NOT NULL,
   PRIMARY KEY (`attempt_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=201954 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=491882 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Table structure for table `group` */
 
 CREATE TABLE `group` (
   `group_id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`group_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Table structure for table `group_member` */
 
 CREATE TABLE `group_member` (
   `group_id` int(11) NOT NULL,
   `member_id` int(11) NOT NULL,
-  PRIMARY KEY (`group_id`,`member_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`member_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Table structure for table `keycode` */
 
 CREATE TABLE `keycode` (
-  `keycode_id` varchar(255) NOT NULL,
+  `keycode_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `member_id` int(11) NOT NULL,
-  `updated` datetime NOT NULL,
-  PRIMARY KEY (`keycode_id`,`member_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `updated` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Table structure for table `member` */
 
 CREATE TABLE `member` (
   `member_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `type` varchar(255) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `apricot_admin` tinyint(1) NOT NULL,
   `joined` datetime NOT NULL,
   `expires` datetime NOT NULL,
   `updated` datetime NOT NULL,
   PRIMARY KEY (`member_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*Table structure for table `network_event` */
 
@@ -79,13 +79,16 @@ CREATE TABLE `network_event` (
 
 CREATE TABLE `reader` (
   `reader_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `timeout` int(11) NOT NULL DEFAULT '10',
-  `enabled` tinyint(1) NOT NULL DEFAULT '1',
-  `group_id` int(11) NOT NULL DEFAULT '0',
-  `address` varchar(255) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `timeout` int(11) NOT NULL DEFAULT 10,
+  `enabled` tinyint(1) NOT NULL DEFAULT 1,
+  `group_id` int(11) NOT NULL DEFAULT 0,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `version` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `settings` varchar(8192) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `status` varchar(4096) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`reader_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
