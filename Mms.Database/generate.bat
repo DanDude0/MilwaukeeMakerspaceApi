400b2fa91bf934dcbe4799d5ec73cd0862d94862
Dan_Dude <dan@vfdworld.com>
2025-02-06 22:23:38 -0600

    Update database mapping

--- Mms.Database/generate.bat
+++ Mms.Database/generate.bat
@@ -1,3 +1,3 @@
-dotnet "%USERPROFILE%/.nuget/packages/petapoco.dbentitygenerator/0.1.0/tools/netcoreapp2.2/PetaPoco.DBEntityGenerator.dll" --config access_control.json > access_control.cs
-dotnet "%USERPROFILE%/.nuget/packages/petapoco.dbentitygenerator/0.1.0/tools/netcoreapp2.2/PetaPoco.DBEntityGenerator.dll" --config area_funding.json > area_funding.cs
-dotnet "%USERPROFILE%/.nuget/packages/petapoco.dbentitygenerator/0.1.0/tools/netcoreapp2.2/PetaPoco.DBEntityGenerator.dll" --config billing.json > billing.cs
+dotnet "C:\Projects\PetaPoco.DBEntityGenerator\PetaPoco.DBEntityGenerator\bin\Debug\net8.0\PetaPoco.DBEntityGenerator.dll" --config access_control.json > access_control.cs
+dotnet "C:\Projects\PetaPoco.DBEntityGenerator\PetaPoco.DBEntityGenerator\bin\Debug\net8.0\PetaPoco.DBEntityGenerator.dll" --config area_funding.json > area_funding.cs
+dotnet "C:\Projects\PetaPoco.DBEntityGenerator\PetaPoco.DBEntityGenerator\bin\Debug\net8.0\PetaPoco.DBEntityGenerator.dll" --config billing.json > billing.cs
